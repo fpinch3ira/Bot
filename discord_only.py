4d64233b4a1a277c27fdb89acb2805037c338dde
fpinch3ira <pincheira.felipe@gmail.com>
2026-04-14 22:41:06 -0400

    commit

--- discord_only.py
+++ discord_only.py
@@ -13,7 +13,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client)
+    print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
